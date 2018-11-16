9314559cfd6c3de19881ce6a932f2da571aab5d7
luoqiang837 <837750861@qq.com>
2018-11-16 13:12:17 +0800

    0.3.8 11-16 整体界面完善

--- app/src/main/java/cn/shenzhenlizuosystemapp/Common/UI/SettingActivity.java
+++ app/src/main/java/cn/shenzhenlizuosystemapp/Common/UI/SettingActivity.java
@@ -1,9 +1,16 @@
 package cn.shenzhenlizuosystemapp.Common.UI;
 
+import android.view.View;
+import android.widget.TextView;
+
 import cn.shenzhenlizuosystemapp.Common.Base.BaseActivity;
+import cn.shenzhenlizuosystemapp.Common.Base.ViewManager;
 import cn.shenzhenlizuosystemapp.R;
 
 public class SettingActivity extends BaseActivity {
+
+    private TextView Back;
+
     @Override
     protected int inflateLayout() {
         return R.layout.setting_layout;
@@ -11,11 +18,22 @@ public class SettingActivity extends BaseActivity {
 
     @Override
     public void initData() {
-
+        InitClick();
     }
 
     @Override
     public void initView() {
+        Back = $(R.id.Back);
+    }
 
+    public void InitClick() {
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                ViewManager.getInstance().finishActivity(SettingActivity.this);
+            }
+
+        });
     }
 }
