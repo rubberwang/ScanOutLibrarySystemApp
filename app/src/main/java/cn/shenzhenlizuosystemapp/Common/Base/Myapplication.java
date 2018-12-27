3bc76da1367f2952185aba75a231ec39e333d07d
xiaoqiang837 <837750861@qq.com>
2018-12-27 19:34:55 +0800

    0.7.9 轮播图片广告

--- app/src/main/java/cn/shenzhenlizuosystemapp/Common/Base/Myapplication.java
+++ app/src/main/java/cn/shenzhenlizuosystemapp/Common/Base/Myapplication.java
@@ -7,18 +7,29 @@ import android.util.Log;
 import com.vise.log.ViseLog;
 import com.vise.log.inner.LogcatTree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.shenzhenlizuosystemapp.R;
+
 
 //import com.squareup.leakcanary.LeakCanary;
 
 public class Myapplication extends Application {
 
     private static Context BaseApplication;
-    public Myapplication myapplication;
+    public static Myapplication myapplication;
+    public static List<?> images=new ArrayList<>();
 
     @Override
 
     public void onCreate() {
         super.onCreate();
+        myapplication = this;
+        Integer[]resource = {R.drawable.lizi3,R.drawable.lizi,R.drawable.lizi2};
+        List list = Arrays.asList(resource);
+        images = new ArrayList(list);
         BaseApplication = getApplicationContext();
         ViseLog.getLogConfig()
                 .configAllowLog(true)//是否输出日志
--- app/src/main/java/cn/shenzhenlizuosystemapp/Common/Fragment/WMS_Fragment.java
+++ app/src/main/java/cn/shenzhenlizuosystemapp/Common/Fragment/WMS_Fragment.java
@@ -1,5 +1,6 @@
 package cn.shenzhenlizuosystemapp.Common.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,9 +12,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
+import com.youth.banner.loader.ImageLoader;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import cn.shenzhenlizuosystemapp.Common.Adapter.Wms_RvAdapter;
 import cn.shenzhenlizuosystemapp.Common.DataAnalysis.WmsSelectData;
@@ -21,18 +28,22 @@ import cn.shenzhenlizuosystemapp.Common.UI.DirectAllot.AllotNotificationActivity
 import cn.shenzhenlizuosystemapp.Common.UI.Input_NotificationActivity;
 import cn.shenzhenlizuosystemapp.Common.UI.Quit_NotificationActivity;
 import cn.shenzhenlizuosystemapp.Common.UI.Check_NotificationActivity;
+import cn.shenzhenlizuosystemapp.Common.View.*;
+import cn.shenzhenlizuosystemapp.Common.Base.Myapplication;
 import cn.shenzhenlizuosystemapp.R;
+
+import static cn.shenzhenlizuosystemapp.Common.Base.Myapplication.myapplication;
 
 public class WMS_Fragment extends Fragment {
 
     public static final String ARGS_PAGE = "WMS_Page";
     private RecyclerView Rv_WmsModuleSelect;
     private TextView Back;
-
+    //private  List<Integer> images;
 
     private ArrayList<WmsSelectData> List_wmsSelectData;
-    private String[] Describe = {"入库作业", "出库作业", "直接调拨", "盘点作业","入库调拨","出库调拨"};
-    private int[] R_Img = {R.drawable.gethouse, R.drawable.puthouse, R.drawable.changehouse, R.drawable.pdacheck,R.drawable.changeget,R.drawable.changeput};
+    private String[] Describe = {"入库作业", "出库作业", "直接调拨", "盘点作业","调拨出库","调拨入库"};
+    private int[] R_Img = {R.drawable.gethouse, R.drawable.puthouse, R.drawable.changehouse, R.drawable.pdacheck,R.drawable.changeput,R.drawable.changeget};
 
     public static MES_Fragment newInstance() {
         MES_Fragment fragment = new MES_Fragment();
@@ -44,15 +55,26 @@ public class WMS_Fragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    public class GlideImageLoader extends ImageLoader {
+        public void displayImage(Context context, Object path, ImageView imageView){
+            Glide.with(context).load(path).into(imageView);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_wms, container, false);
         Rv_WmsModuleSelect = rootView.findViewById(R.id.Rv_WmsModuleSelect);
         Back = rootView.findViewById(R.id.Back);
+        Banner banner = rootView.findViewById(R.id.banner);
+        banner.setImageLoader(new GlideImageLoader());
+        banner.setImages(myapplication.images);
+        banner.start();
         List_wmsSelectData = new ArrayList<>();
         InitRecycler();
         InitClick();
+
         return rootView;
     }
 
